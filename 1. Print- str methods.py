582d3aba76f0860e45eaaa040437a30070d2fbfa
İbrahim Rüzkareş <138830968+ibrahimruzkares@users.noreply.github.com>
2024-04-06 00:45:30 +0300

    Add files via upload

---
+++ 1. Print- str methods.py
@@ -0,0 +1,9 @@
+text = "Python for begginners"
+print(len(text))
+print(text.upper())
+print(text.lower())
+print(text.title())
+print(text.find("f"))
+print(text.replace("Python" , "Piton"))
+print("Python" in text)
+print(text[0:5])
